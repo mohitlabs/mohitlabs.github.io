6c0c78a2004153cab251e3bd4b750e4e73cefb73
MohitLabs <mohitsingh5.ms@gmail.com>
2023-06-30 00:53:05 +0530

    commit

--- assets/js/theme-load.js
+++ assets/js/theme-load.js
@@ -1,45 +1,47 @@
 // Get the user's theme preference from the browser's local storage
-let themePreference = localStorage.getItem("theme");
-let toggleThemeCheckbox = document.querySelector("#toggle-theme-checkbox");
+const themePreference = localStorage.getItem("theme");
 
 if (!document.getElementById("theme-stylesheet")) {
-    // Create and append the preload link element
-    let preloadLinkDark = document.createElement("link");
-    preloadLinkDark.rel = "preload";
-    preloadLinkDark.href = "/assets/css/dark.css";
-    preloadLinkDark.as = "style";
+    // Check if user preference is stored in localStorage
+    if (themePreference) {
+        setTheme(themePreference);
+    } else {
+        // By default, use the system preference
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            setTheme("light");
+        } else {
+            setTheme("dark");
+        }
+    }
+}
 
-    document.head.appendChild(preloadLinkDark);
-
-    let preloadLinkLight = document.createElement("link");
-    preloadLinkLight.rel = "preload";
-    preloadLinkLight.href = "/assets/css/light.css";
-    preloadLinkLight.as = "style";
-
-    document.head.appendChild(preloadLinkLight);
-
+function setTheme(mode) {
     // Create a link element
     let linkElement = document.createElement("link");
 
     // Set the attributes of the link element
     linkElement.id = "theme-stylesheet";
     linkElement.rel = "stylesheet";
-    linkElement.href = "/assets/css/" + themePreference + ".css";
+    linkElement.href = "/assets/css/" + mode + ".css";
 
     // Append the link element to the head section of your HTML document
     document.head.appendChild(linkElement);
+    localStorage.setItem("theme", mode);
 }
 
 // Wait for DOM content load
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("theme-stylesheet").onload = () => {
-        document.getElementById("app-root").classList.remove("d-none");
-    }
-    if (themePreference === "dark") {
+    let toggleThemeCheckbox = document.querySelector("#toggle-theme-checkbox");
+
+    if (localStorage.getItem("theme") === "dark") {
         toggleThemeCheckbox.checked = true;
         toggleThemeCheckbox.title = "Toggle Light Mode";
     } else {
         toggleThemeCheckbox.checked = false;
         toggleThemeCheckbox.title = "Toggle Dark Mode";
+    }
+
+    document.getElementById("theme-stylesheet").onload = () => {
+        document.getElementById("app-root").classList.remove("d-none");
     }
 });
